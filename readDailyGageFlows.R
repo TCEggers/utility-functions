065db17c8544c4c26fc56ddae8e857ca6fc2bb5c
swrcb-dwr-enf-wueu <Jeffrey.Yeazell@waterboards.ca.gov>
2016-07-14 15:19:13 -0700

    Changed some sensor ids

--- readDailyGageFlows.R
+++ readDailyGageFlows.R
@@ -1,37 +1,37 @@
 ##' readDailyGageFlows
 ##' Version: 1.1
 ##' Modified: 2016-06-16
-##' 
-##' Scrapes and returns daily gage flow data in cubic feet per second (cfs) from 
-##' DWR's CDEC web site for stations passed to the function in the 
-##' 'station_sensor_list' data frame. If no station list is passed, the function 
-##' defaults to downloading data for the stations applicable to the Sacramento, 
-##' San Joaquin, and Delta analysis areas. If multiple measurements are reported 
-##' for a station in a day (i.e., at hourly or quarterly intervals), the 
+##'
+##' Scrapes and returns daily gage flow data in cubic feet per second (cfs) from
+##' DWR's CDEC web site for stations passed to the function in the
+##' 'station_sensor_list' data frame. If no station list is passed, the function
+##' defaults to downloading data for the stations applicable to the Sacramento,
+##' San Joaquin, and Delta analysis areas. If multiple measurements are reported
+##' for a station in a day (i.e., at hourly or quarterly intervals), the
 ##' function returns the daily average.
-##' 
-##' At a minimum, the station_sensor_list data frame is expected to have the 
+##'
+##' At a minimum, the station_sensor_list data frame is expected to have the
 ##' following three columns (one rowfor each station):
 ##'     station_id: Three-character station id code
 ##'     sensor: The sensor number for the data set requested. Typically it is 20
 ##'             for River discharge in cfs, but should be confirmed on CDEC.
-##'     dur_code:   Duration code. Typically "E" (event) or "H" (hourly), but should 
+##'     dur_code:   Duration code. Typically "E" (event) or "H" (hourly), but should
 ##'                 be confirmed on CDEC.
-##' 
+##'
 ##' The following columns are optional:
 ##'     station_name: The station's name
 ##'     sensor_description: Type of measurement the sensor reports
-##' 
+##'
 ##' The function returns a tidy-long data frame with the following columns:
 ##'     date: Measurement date
 ##'     station_id: Three-character station id code
 ##'     variable: Measurement variable, in this case, 'gage_flow_cfs'
 ##'     value: value of the measurement variable.
-##' 
+##'
 ##' Changes:
 ##'   2016-06-09: Converted variable names to lower-case_underscore style
 ##'               Rewrote code so that resulting data frame is tidy-long
-##'   2016-06-16: Renamed function to readDailyGageFlows                   
+##'   2016-06-16: Renamed function to readDailyGageFlows
 ##'
 
 # Load required libraries during source if not yet loaded ----
@@ -49,7 +49,7 @@ readDailyGageFlows <- function(station_sensor_list = NA,
                           start_date = "2015-10-01",
                           end_date = "Now",
                           save_csv = TRUE) {
-    
+
     # If no station_sensor_list is passed to the function, use the default set, use
     # the 21 stations mapped for the supply/demand network analysis:
     if (is.na(station_sensor_list)) {
@@ -57,67 +57,67 @@ readDailyGageFlows <- function(station_sensor_list = NA,
             station_id = c("BND", "ORO", "YRS", "MRY", "FSB", "VON", "FOL", "FPT",
                            "SJF", "SMN", "NEW", "MMF", "LGN", "MOD", "VNS", "GDW",
                            "OBB", "KOT", "PAR", "CMN", "MHB"),
-            station_name = c("Sacramento R. at Bend Bridge", 
+            station_name = c("Sacramento R. at Bend Bridge",
                              "Oroville Dam (Feather R.)",
-                             "Yuba R. near Smartvillle", 
+                             "Yuba R. near Smartvillle",
                              "Yuba R. near Marysville",
-                             "Feather R. above Star Bend", 
+                             "Feather R. above Star Bend",
                              "Sacramento R. at Verona",
-                             "Folsom Lake (Sacramento R.)", 
+                             "Folsom Lake (Sacramento R.)",
                              "Sacramento R. at Freeport",
-                             "San Joaquin R. below Friant", 
+                             "San Joaquin R. below Friant",
                              "San Joaquin R. above Merced near Newman",
-                             "San Joaquin R. near Newman", 
+                             "San Joaquin R. near Newman",
                              "Merced R. below Merced Falls",
                              "Tuolumne R. below La Grange Dam near La Grange",
                              "Tuolumne R. at Modesto",
-                             "San Joaquin R. near Vernalis", 
+                             "San Joaquin R. near Vernalis",
                              "Goodwin Dam (Stanislaus R.)",
-                             "Stanislaus R. at Orange Blossom Bridge", 
+                             "Stanislaus R. at Orange Blossom Bridge",
                              "Stanislaus R. at Koetitz Ranch",
-                             "Pardee (Mokelumne R.)", 
+                             "Pardee (Mokelumne R.)",
                              "Camanche Reservoir (Mokelumne R.)",
                              "Cosumnes R. at Michigan Bar"),
-            sensor = c(20, 23, 20, 20, 20, 20, 23, 20, 20, 20, 20, 20, 20, 20, 20,
+            sensor = c(41, 23, 41, 20, 20, 20, 23, 20, 20, 20, 20, 20, 41, 20, 20,
                        71, 20, 20, 23, 23, 20),
-            dur_code = c("H", "H", "H", "E", "E", "H", "H", "E", "E", "E", "H", "H",
-                         "H", "E", "H", "D", "H", "E", "H", "H", "H"),
-            sensor_description = c("Flow, River Discharge (cfs)", 
-                                   "Reservoir Ouflow (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Reservoir Ouflow (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Discharge, Spillway (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Flow, River Discharge (cfs)", 
-                                   "Reservoir Ouflow (cfs)", 
-                                   "Reservoir Ouflow (cfs)", 
+            dur_code = c("D", "D", "D", "E", "E", "H", "D", "E", "E", "E", "H", "H",
+                         "D", "E", "H", "D", "H", "E", "H", "H", "H"),
+            sensor_description = c("Flow, Mean Daily (cfs)",
+                                   "Reservoir Ouflow (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, Mean Daily (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Reservoir Ouflow (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, Mean Daily (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Discharge, Spillway (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Flow, River Discharge (cfs)",
+                                   "Reservoir Ouflow (cfs)",
+                                   "Reservoir Ouflow (cfs)",
                                    "Flow, River Discharge (cfs)"),
             stringsAsFactors = FALSE
         )
     }
-    
+
     scrape_start <- paste(month(start_date),"%2F", day(start_date),"%2F", year(start_date), sep = "")
-    
+
     if (end_date == "Now") {
         scrape_end <- "Now"
         end_date <- today()
     } else {
         scrape_end <- paste(month(end_date),"%2F", day(end_date),"%2F", year(end_date), sep = "")
     }
-    
+
     gage_flows <- list()
-    
+
     for (i in 1 : nrow(station_sensor_list)) {
         cat("Processing Station ", station_sensor_list$station_id[i] ,"...\n", sep = "")
         source_url <- paste("http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=",
@@ -128,35 +128,35 @@ readDailyGageFlows <- function(station_sensor_list = NA,
                             "&end_date=", scrape_end,
                             "&data_wish=View+CSV+Data",
                             sep = "")
-        gage_flows[[i]] <- (read.csv(source_url, 
-                                     skip = 1, 
+        gage_flows[[i]] <- (read.csv(source_url,
+                                     skip = 1,
                                      quote = "\'",
                                      na.strings = "m",
                                      stringsAsFactors = FALSE))
         names(gage_flows[[i]]) <- c("date", "time","gage_flow_cfs")
-        gage_flows[[i]] <- gage_flows[[i]] %>% 
+        gage_flows[[i]] <- gage_flows[[i]] %>%
             mutate(station_id = station_sensor_list$station_id[i]) %>%
             select(station_id, everything())
     }
-    
+
     gage_flows <- bind_rows(gage_flows)
     gage_flows$date <- as.Date(as.character(gage_flows$date), "%Y%m%d")
-    
-    daily_gage_flows <- gage_flows %>% 
+
+    daily_gage_flows <- gage_flows %>%
         group_by(station_id, date) %>%
         summarize(gage_flow_cfs = mean(gage_flow_cfs, na.rm =TRUE))%>%
         as.data.frame(.)
-    
+
     # Tidy data frame:
-    daily_gage_flows <- daily_gage_flows %>% 
+    daily_gage_flows <- daily_gage_flows %>%
         gather(variable, value, -date, -station_id) %>%
         select(date, station_id, variable, value)
-    
+
     cat("\nWeb scrape complete.\n\n")
-    
+
     if(save_csv){
         if(!file.exists("./Daily_Gage_Flow_Downloads")) dir.create("./Daily_Gage_Flow_Downloads")
-        fileDest <- sprintf("./Daily_Gage_Flow_Downloads/Gage_Flows_%s.csv", 
+        fileDest <- sprintf("./Daily_Gage_Flow_Downloads/Gage_Flows_%s.csv",
                             format(Sys.time(),"%Y%m%d_%H%M"))
         write.csv(daily_gage_flows, fileDest, row.names = FALSE)
         cat("\nData file saved to ",fileDest,"\n\n")
